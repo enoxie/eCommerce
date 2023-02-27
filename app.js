21bffb27e035ea2d76c4ba0b14eafde20e99acca
enoxie <admin@enesbilgi.com>
2023-02-28 02:19:22 +0300

    Thora v.0.0.2-alpha

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-const button = document.getElementById("shop");
-const header = document.getElementById("head");
-button.addEventListener("click", function () {
-  header.classList.add("stuck");
-});
+// ? Navbar Position
+// Kullanıcı scroll down yaptığı zaman bu fonksiyon çalışacak
+window.onscroll = function () {
+  stickyHeader();
+};
+
+// Header alıyoruz.
+var header = document.getElementById("head");
+
+// headerın pozisyonu
+var sticky = header.offsetTop;
+
+// Eğer header pozisyonu geçiliyorsa class ekleniyor, geçmiyorsa class kaldırılıyor
+function stickyHeader() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("header-sticky");
+  } else {
+    header.classList.remove("header-sticky");
+  }
+}
