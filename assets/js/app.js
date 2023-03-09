a0a70decee13a4bb0f7a042eb00d34f2de5b0463
enoxie <admin@enesbilgi.com>
2023-03-09 19:55:33 +0300

    Thora v.0.0.3-alpha

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,17 +4,51 @@ window.onscroll = function () {
   stickyHeader();
 };
 
-// Header alıyoruz.
+/*  Elements alıyoruz.*/
+let isOpenSearch = false;
 var header = document.getElementById("head");
+var btnSearch = document.getElementById("btn-search");
+var searchDropdown = document.getElementById("search-dropdown");
+var searchInput = document.getElementById("search-input");
+var searchClose = document.getElementById("btn-search-close");
+
+/* Search Box Events Begin */
+btnSearch.addEventListener("click", function () {
+  isOpenSearch ? (isOpenSearch = false) : (isOpenSearch = true);
+  if (isOpenSearch) {
+    searchDropdown.style.display = "flex";
+    searchInput.focus();
+    header.classList.remove("header-sticky");
+    header.classList.add("search-sticky");
+  } else {
+    searchDropdown.style.display = "none";
+  }
+});
+
+searchClose.addEventListener("click", function () {
+  searchDropdown.style.display = "none";
+  isOpenSearch = false;
+  header.classList.add("header-sticky");
+  header.classList.remove("search-sticky");
+});
+/* Search Box Events End*/
 
 // headerın pozisyonu
 var sticky = header.offsetTop;
 
 // Eğer header pozisyonu geçiliyorsa class ekleniyor, geçmiyorsa class kaldırılıyor
 function stickyHeader() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("header-sticky");
+  if (!isOpenSearch) {
+    if (window.pageYOffset > sticky) {
+      header.classList.add("header-sticky");
+    } else {
+      header.classList.remove("header-sticky");
+    }
   } else {
-    header.classList.remove("header-sticky");
+    if (window.pageYOffset > sticky) {
+      header.classList.add("search-sticky");
+    } else {
+      header.classList.remove("search-sticky");
+    }
   }
 }
